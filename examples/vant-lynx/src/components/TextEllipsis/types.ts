e2b6c2a7ca88ddcf28b612cc0c83ad8cc0da7736
xuan.huang <5563315+Huxpro@users.noreply.github.com>
2026-03-22 11:12:38 -0400

    feat(text-ellipsis): 100% feature parity with Vant

---
+++ examples/vant-lynx/src/components/TextEllipsis/types.ts
@@ -0,0 +1,8 @@
+export type TextEllipsisExpose = {
+  toggle: (expanded?: boolean) => void;
+};
+
+export type TextEllipsisThemeVars = {
+  textEllipsisActionColor?: string;
+  textEllipsisLineHeight?: string | number;
+};
