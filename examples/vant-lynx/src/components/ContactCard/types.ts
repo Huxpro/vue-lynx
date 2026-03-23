32d9bf240c6389ed7eb0dba66e7f4a4d69d41342
xuan.huang <5563315+Huxpro@users.noreply.github.com>
2026-03-22 22:35:37 -0400

    feat(contact-card): 100% feature parity with Vant

---
+++ examples/vant-lynx/src/components/ContactCard/types.ts
@@ -0,0 +1,8 @@
+export type ContactCardType = 'add' | 'edit';
+
+export type ContactCardThemeVars = {
+  contactCardPadding?: string;
+  contactCardAddIconSize?: string;
+  contactCardAddIconColor?: string;
+  contactCardTitleLineHeight?: number | string;
+};
